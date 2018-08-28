82ea8ba7a068c48e905546bae460decc1368ab8d
Chen, JasonX Z <jasonx.z.chen@intel.com>
2018-08-28 01:59:24 -0400

    [tensorflow] upload pb fild which can classify 4 kinds of clothes
    upload pb file and labels.txt
    upload run-test script
    
    Signed-off-by: Chen, JasonX Z <jasonx.z.chen@intel.com>

---
+++ build_pb_file/tmp-jc-label-script.sh
@@ -0,0 +1,2 @@
+python label_image.py --graph=/tmp/output_graph.pb --labels=/tmp/output_labels.txt --input_layer=Placeholder --output_layer=final_result --image=examples/image_retraining/training-data/blazer/pic_001.jpg
+
